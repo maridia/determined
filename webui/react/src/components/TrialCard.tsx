f16bc0d3c4afa961c46bb050b57a5656b8835beb
Hamid Zare <hamid@determined.ai>
2020-11-12 11:55:37 -0800

    wip make tasc card vertical

--- webui/react/src/components/TrialCard.tsx
+++ webui/react/src/components/TrialCard.tsx
@@ -25,7 +25,7 @@ const configRenderer = (conf: RawJson, path?: string[]) => {
   // TODO maybe use monaco editor in readonly mode
   if (path === undefined) return;
   const subConf = path.length ? { [path[path.length-1]]: getPathList(conf, path) } : conf;
-  return <MonacoEditor
+  const confA = <MonacoEditor
     height="20rem"
     language="json"
     options={{
@@ -38,37 +38,49 @@ const configRenderer = (conf: RawJson, path?: string[]) => {
     }}
     theme="vs-light"
     value={JSON.stringify(subConf, null, 2)} />;
-  // return <pre>{JSON.stringify(subConf, null, 2)}</pre>;
+  const confB = <pre>{JSON.stringify(subConf, null, 2)}</pre>;
+  return confB;
 };
 
 const TrialCard: React.FC<Props> = ({ trial, experiment, ...p }: Props) => {
   const durations = useMemo(() => trialDurations(trial.steps), [ trial.steps ]);
 
-  return <Section bodyBorder title={p.title || `Trial ${trial.id} Card`}>
-    <Breadcrumb>
-      <Breadcrumb.Item>
-        <Link path={`/experiments/${experiment.id}`}>Experiment {experiment.id}</Link>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item>
-        <Link path={`/trials/${trial.id}`}>Trial {trial.id}</Link>
-      </Breadcrumb.Item>
-    </Breadcrumb>
+  return <Section
+    bodyBorder
+    options={
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link path={`/experiments/${experiment.id}`}>Experiment {experiment.id}</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link path={`/trials/${trial.id}`}>Trial {trial.id}</Link>
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    }
+    title={p.title || `Trial ${trial.id} Card`}
+  >
+    <div className={css.body}>
+      <p>stats</p>
+      <p>experiment launched {experiment.trials.length} trials</p>
 
-    <div>
-      <h5>Config</h5>
-      {configRenderer(experiment.configRaw, p.configPath)}
+      <div className={css.duration}>
+        <h5>Durations</h5>
+        <div>Training: {shortEnglishHumannizer(durations.train)}</div>
+        <div>Checkpointing: {shortEnglishHumannizer(durations.checkpoint)}</div>
+        <div>Validating: {shortEnglishHumannizer(durations.validation)}</div>
+        {/* <TrialInfoBox experiment={experiment} trial={trial} /> */}
+      </div>
+
+      {p.configPath &&
+      <div>
+        <h5>Config</h5>
+        <p>some text about this</p>
+        {configRenderer(experiment.configRaw, p.configPath)}
+      </div>
+      }
+
+      <TrialChart steps={trial.steps} {...p.trialChartProps} />
     </div>
-
-    <div className={css.duration}>
-      <h5>Durations</h5>
-      <div>Training: {shortEnglishHumannizer(durations.train)}</div>
-      <div>Checkpointing: {shortEnglishHumannizer(durations.checkpoint)}</div>
-      <div>Validating: {shortEnglishHumannizer(durations.validation)}</div>
-    </div>
-
-    <TrialChart steps={trial.steps} {...p.trialChartProps} />
-    <h5>Stats</h5>
-    <p>experiment launched {experiment.trials.length} trials</p>
 
     {/* <TrialInfoBox experiment={experiment} trial={trial} /> */}
   </Section>;
