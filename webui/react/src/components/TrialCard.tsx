937d289ce51cd25e04811e944e5bd85ec17c4be5
Hamid Zare <hamid@determined.ai>
2020-11-12 11:08:15 -0800

    add durations and more interesting metrics
    add a request to fetch trial data

--- webui/react/src/components/TrialCard.tsx
+++ webui/react/src/components/TrialCard.tsx
@@ -1,11 +1,16 @@
 import { Breadcrumb } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 
 import Link from 'components/Link';
 import Section from 'components/Section';
 import TrialChart, { Props as TrialChartProps } from 'pages/TrialDetails/TrialChart';
+import TrialInfoBox from 'pages/TrialDetails/TrialInfoBox';
 import { ExperimentDetails, RawJson, TrialDetails } from 'types';
 import { getPathList } from 'utils/data';
+import { shortEnglishHumannizer } from 'utils/time';
+import { trialDurations } from 'utils/types';
+
+import css from './TrialCard.module.scss';
 
 interface Props {
   trial: TrialDetails;
@@ -21,7 +26,9 @@ const configRenderer = (conf: RawJson, path?: string[]) => {
 };
 
 const TrialCard: React.FC<Props> = ({ trial, experiment, ...p }: Props) => {
-  return <Section title={`Trial ${trial.id}`}>
+  const durations = useMemo(() => trialDurations(trial.steps), [ trial.steps ]);
+
+  return <Section bodyBorder title={`Trial ${trial.id} Card`}>
     <Breadcrumb>
       <Breadcrumb.Item>
         <Link path={`/experiments/${experiment.id}`}>Experiment {experiment.id}</Link>
@@ -31,10 +38,20 @@ const TrialCard: React.FC<Props> = ({ trial, experiment, ...p }: Props) => {
       </Breadcrumb.Item>
     </Breadcrumb>
 
-    <p>stats</p>
+    <p>config</p>
     {configRenderer(experiment.configRaw, p.configPath)}
-    <p> of config</p>
     <TrialChart steps={trial.steps} {...p.trialChartProps} />
+    <p>stats</p>
+    <p>experiment launched {experiment.trials.length} trials</p>
+
+    {/* <TrialInfoBox experiment={experiment} trial={trial} /> */}
+
+    <p>Durations</p>
+    <div className={css.duration}>
+      <div>Training: {shortEnglishHumannizer(durations.train)}</div>
+      <div>Checkpointing: {shortEnglishHumannizer(durations.checkpoint)}</div>
+      <div>Validating: {shortEnglishHumannizer(durations.validation)}</div>
+    </div>,
 
   </Section>;
 };
--- webui/react/src/pages/Report.tsx
+++ webui/react/src/pages/Report.tsx
@@ -2,24 +2,27 @@ import { Space } from 'antd';
 import React, { useEffect, useState } from 'react';
 
 import Logo, { LogoTypes } from 'components/Logo';
-import Message, { MessageType } from 'components/Message';
 import Page from 'components/Page';
 import Spinner from 'components/Spinner';
 import TaskCard from 'components/TaskCard';
 import TrialCard from 'components/TrialCard';
-import { getExperimentDetails } from 'services/api';
+import { getExperimentDetails, getTrialDetails } from 'services/api';
 import { ExperimentDetails, MetricType, TrialDetails } from 'types';
 import { experimentDetailsToTask } from 'utils/types';
 
 import css from './Report.module.scss';
 
 type Data = Record<number, ExperimentDetails>;
+type TrialData = Record<number, TrialDetails>;
 
-const targetExps = [ 2 ];
+const targetExps = [ 10, 2 ];
+const targetTrials = [ 10 ];
 
 const Report: React.FC = () => {
   const [ expDetails, setExpDetails ] = useState<Data>({});
+  const [ trialDetails, setTrialDetails ] = useState<TrialData>({});
 
+  // fetch initial data.
   useEffect(() => {
     Promise.all(targetExps.map(expId => getExperimentDetails({ id: expId })))
       .then(details => {
@@ -29,11 +32,21 @@ const Report: React.FC = () => {
         }, {});
         setExpDetails(newExpDetails);
       }).catch(console.error);
-  }, [ setExpDetails ]);
 
-  const exp1 = expDetails[targetExps[0]];
+    Promise.all(targetTrials.map(expId => getTrialDetails({ id: expId })))
+      .then(details => {
+        const newTrialDetails = details.reduce((acc: TrialData, cur: TrialDetails) => {
+          acc[cur.id] = cur;
+          return acc;
+        }, {});
+        setTrialDetails(newTrialDetails);
+      }).catch(console.error);
+  }, [ setExpDetails, setTrialDetails ]);
 
-  if (!exp1) return <Spinner />;
+  const exp0 = expDetails[targetExps[0]]; // exp with index 0 in targetExps
+  const trial0 = trialDetails[targetTrials[0]];
+
+  if (!exp0 || !trial0) return <Spinner />;
 
   return (
     <Page
@@ -44,20 +57,20 @@ const Report: React.FC = () => {
       title="Training CycleGAN using Determined">
       <p>CycleGAN is a technique that can be used to do image-to-image translation. In this report, I've outlined how I trained CycleGAN using Determined.</p>
       <p>The first step is to try to optimize for the maximum batch size, which I did in this experiment:</p>
-      <p>{exp1?.state}</p>
-      <TaskCard {...experimentDetailsToTask(exp1)}
+      <p>{exp0?.state}</p>
+      <TaskCard {...experimentDetailsToTask(exp0)}
       />
 
       <div className={css.readme}>
         <p>next message</p>
       </div>
       <TrialCard
-        configPath={[ 'hyperparameters' ]}
-        experiment={exp1}
-        trial={{ id: 2 } as TrialDetails}
+        configPath={[ 'searcher' ]}
+        experiment={exp0}
+        trial={trial0}
         trialChartProps={{
-          defaultMetricNames: [ { name: 'loss_real_D', type: MetricType.Validation } ],
-          metricNames: [ { name: 'loss_real_D', type: MetricType.Validation } ],
+          defaultMetricNames: [ { name: 'validation_error', type: MetricType.Validation } ],
+          metricNames: [ { name: 'validation_error', type: MetricType.Validation } ],
         }} />
 
       <div className={css.readme}>
